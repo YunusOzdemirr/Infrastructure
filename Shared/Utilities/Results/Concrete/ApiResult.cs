f016f4cef6bf3e557f56a0f3202de85bde2c50c9
Yunus <yunusozdemir468@gmail.com>
2021-10-05 22:07:04 +0300

    DataResult added

---
+++ Shared/Utilities/Results/Concrete/ApiResult.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Shared.Entities.ComplexTypes;
+using Shared.Utilities.Results.ComplexTypes;
+
+namespace Shared.Utilities.Results.Concrete
+{
+    public class ApiResult
+    {
+        public ApiResult(ResultStatus resultStatus,string message,HttpStatusCode httpStatusCode,string href)
+        {
+
+        }
+        public ApiResult()
+        {
+
+        }
+        public ResultStatus ResultStatus { get; set; }
+        public string Message { get; set; }
+        public HttpStatusCode HttpStatusCode { get; set; }
+        public string Href { get; set; }
+    }
+}
+
