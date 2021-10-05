cfc7e3520b954b9114a76c653260da24a2e49b36
Yunus <yunusozdemir468@gmail.com>
2021-10-05 22:44:45 +0300

    AccessToken andAccessTokenOptions added

--- Shared/Utilities/Validation/DateConflictValidator.cs
+++ Shared/Utilities/Validation/DateConflictValidator.cs
@@ -3,8 +3,13 @@ namespace Shared.Utilities.Validation
 {
     public class DateConflictValidator
     {
-        public DateConflictValidator()
+        public static bool CheckDateConflict(DateTime start, DateTime end, DateTime query)
         {
+            if (query.Ticks>start.Ticks && query.Ticks<end.Ticks)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
